0857042f77c15cac8eec3e862a1ffaf81dc1f98c
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-19 03:41:44 -0300

    Add some new types with conditional compilation

--- src/win_types.rs
+++ src/win_types.rs
@@ -1,6 +1,23 @@
 // Custom typedefs, only those that this project uses
 // Reference: https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
 
+use std::ffi::c_void;
+
+#[cfg(target_arch = "x86")]
+use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS32;
+#[cfg(target_arch = "x86_64")]
+use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64;
+
+pub type ULONG_PTR = u64;
+pub type DWORD_PTR = ULONG_PTR;
 pub type DWORD = u32;
 pub type WORD = u16;
 pub type LCID = DWORD;
+pub type LPVOID = *const c_void;
+pub type SIZE_T = usize;
+pub type LPCVOID = *const c_void;
+
+#[cfg(target_arch = "x86")]
+pub type IMAGE_NT_HEADER = IMAGE_NT_HEADERS32;
+#[cfg(target_arch = "x86_64")]
+pub type IMAGE_NT_HEADER = IMAGE_NT_HEADERS64;
