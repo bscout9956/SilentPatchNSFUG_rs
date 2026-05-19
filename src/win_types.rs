b84edf09b7be2bba1769f5a61faec90a16a34dae
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-19 00:13:04 -0300

    Use c_char instead of primitive i8s

--- src/win_types.rs
+++ src/win_types.rs
@@ -1,8 +1,10 @@
 // Custom typedefs, only those that this project uses
 // Reference: https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
 
+use std::os::raw::c_char;
+
 pub type DWORD = u32;
 pub type WORD = u16;
 pub type LCID = DWORD;
-pub type LPCSTR = *const i8;
-pub type LPSTR = *mut i8;
+pub type LPCSTR = *const c_char;
+pub type LPSTR = *mut c_char;
