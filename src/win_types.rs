f6440010a5c347963cf908854a78f2149ebd5fd4
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-18 23:51:19 -0300

    Create win_types.rs

---
+++ src/win_types.rs
@@ -0,0 +1,5 @@
+// Custom typedefs, only those that this project uses
+// Reference: https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
+
+pub type DWORD = u32;
+pub type WORD = u16;
