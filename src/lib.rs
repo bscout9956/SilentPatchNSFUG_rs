5831971dc46b1b9de8b1bc8746649ad432d3682d
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-20 01:37:53 -0300

    Use Pattern once
    One step closer...
    
    (Will generate warnings, WIP)

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,20 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
+use std::ffi::c_void;
+
 use windows_sys::Win32::{Foundation::HMODULE, System::LibraryLoader::GetModuleHandleA};
+
 mod FixedDateFormat;
 mod FixedDriftScore;
 mod Patterns;
 mod ScopedUnprotect;
 mod macros;
 mod win_types;
+
+use Patterns::txn;
+
+use crate::FixedDriftScore::GetTotalLapScore_Hook;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn OnInitializeHook() {
@@ -20,6 +27,11 @@ pub extern "C" fn OnInitializeHook() {
         // Original comment:
         // Fix the drift score magazine taking a best lap score and dividing it by laps.
         // Also fix the high score in the menu displaying style points instead of the full score.
-        // let getBestLapScore = pattern("E8 ? ? ? ? E8 ? ? ? ? 89 86").get_one();
+
+        let getBestLapScore = txn::Pattern::new(b"E8 ? ? ? ? E8 ? ? ? ? 89 86").get_one();
+        let beatingPresetDriftScore: *mut c_void =
+            txn::get_pattern(b"57 E8 ? ? ? ? 83 C4 08 E8", 1);
+
+        // InjectHook(getBestLapScore.get::<c_void>(), GetTotalLapScore_Hook);
     }
 }
