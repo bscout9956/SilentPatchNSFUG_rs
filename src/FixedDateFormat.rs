3fdf15cb0d2ded1d2dcdb2c7052047cb78bcc89a
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-21 01:59:05 -0300

    Autoformat

--- src/FixedDateFormat.rs
+++ src/FixedDateFormat.rs
@@ -84,7 +84,11 @@ pub extern "system" fn GetDateFormatA_GameLanguageFormat(
     cchDate: i32,
 ) -> i32 {
     unsafe {
-        let language: i32 = if CurrentLanguage.is_null() {0} else {*CurrentLanguage};
+        let language: i32 = if CurrentLanguage.is_null() {
+            0
+        } else {
+            *CurrentLanguage
+        };
 
         let mut mutable_lpDate: *const SYSTEMTIME = lpDate;
         let mut local_time: SYSTEMTIME = std::mem::zeroed();
@@ -180,4 +184,11 @@ pub extern "system" fn GetDateFormatA_Fallback(
     }
 }
 
-static mut pGetDateFormatA_SilentPatch: extern "system" fn(LCID, DWORD, *const SYSTEMTIME, PCSTR, PSTR, i32) -> i32 = GetDateFormatA_Fallback;
+static mut pGetDateFormatA_SilentPatch: extern "system" fn(
+    LCID,
+    DWORD,
+    *const SYSTEMTIME,
+    PCSTR,
+    PSTR,
+    i32,
+) -> i32 = GetDateFormatA_Fallback;
